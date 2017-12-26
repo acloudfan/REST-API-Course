18ccc3df0053e1a0599b84d78c141db1f963b657
acloudfan <raj@acloudfan.com>
2017-12-26 07:37:13 -0500

    Update index.js
    DB URI change

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
-process.env.DB_URI = "set this up"
-process.env.DB_USER = "set this up" /**provide the user id */
-process.env.DB_PASSWORD = "set this up" /**provide the password */
+
+
+// Provide the URI in the right format
+process.env.DB_URI = "mongodb://<user>:<password>@SERVER:PORT/DB-NAME"
+
+// Following 2 are not needes as the user/password are part of the URI
+//process.env.DB_USER = "set this up" /**provide the user id */
+//process.env.DB_PASSWORD = "set this up" /**provide the password */
 
 var express = require('express')
 var bodyParser = require('body-parser')
